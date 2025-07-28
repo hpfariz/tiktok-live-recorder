9475b0e3e883ca37839604323dd41f6db9837c6a
hpfariz <36537675+hpfariz@users.noreply.github.com>
2025-07-29 06:28:02 +0700

    chore: change user to b.encer

--- server.py
+++ server.py
@@ -64,7 +64,7 @@ RECORDINGS_DIR.mkdir(parents=True, exist_ok=True)
 # --------------------------------------------------------------------------- #
 def run_recorder():
     Popen(
-        ["python", "main.py", "-user", "gragalbert", "-mode", "automatic"],
+        ["python", "main.py", "-user", "b.encer", "-mode", "automatic"],
         cwd=SRC
     )
 
